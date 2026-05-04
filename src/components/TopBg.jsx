542e1f00f8ec8a0eca4fe3eb60cbb259f5cef8a7
anthony <ikennaanthony777@gmail.com>
2026-05-04 13:57:56 -0700

    third commit

--- src/components/TopBg.jsx
+++ src/components/TopBg.jsx
@@ -1,7 +1,7 @@
 const TopBackgroungImage = () => {
     return ( 
         <div className="bg-green-100">
-            <div className="w-full bg-[url('./images/bg-header-desktop.svg')] h-40 m-0 p-0">
+            <div className="w-full bg-[url('/images/bg-header-desktop.svg')] h-40 m-0 p-0">
 
         </div>
         </div>
